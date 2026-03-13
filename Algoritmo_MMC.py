057ac22e2637030b84defdc3f50732612c24d2e6
Cayo Aragão <cayodavesaragao@gmail.com>
2026-03-13 13:30:46 -0300

    Melhoria: cálculo do mmc de dois ou mais números

--- Algoritmo_MMC.py
+++ Algoritmo_MMC.py
@@ -1,18 +1,16 @@
-# Atribuição de valores
-n1 = 18
-n2 = 20
-
-
-# Função para descobrir o MDC (maior divisor comum) e logo depois retornar o MMC (menor múltiplo comum) entre dois números
-def mmc(n1, n2):
-    n3 = n1*n2
+# Função para descobrir o MDC (maior divisor comum)
+def mdc(n1, n2):
     while n1%n2 != 0:
         n1, n2 = n2, n1%n2
-    return n3 // n2
+    return n2
 
 
-# Armazenamento e exibição do resultado
-resultado = mmc(n1, n2)
-print(f"O MMC destes números é {resultado}")
+# Função para descobrir o MMC de dois ou mais números 
+def mmc(*args):
+    mmc = args[0]
+    for i in range(len(args)):
+        mmc = mmc*args[i]//mdc(mmc, args[i])
+    return mmc
 
-# Próximo passo: possibilitar a descoberta do MMC de 3 números ou mais;
+
+print(mmc(42, 74, 88, 92))
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # MMC
-Este projeto apresenta um algoritmo simples que calcula o MMC de dois números; Primeiro é descoberto o MDC (máximo divisor comum) e logo então é feito o cálculo para descobrir o MMC (menor múltiplo comum)
-mmc=(n1*n2)/mdc;
+Este projeto apresenta um algoritmo simples que calcula o MMC de um conjunto de números; Primeiro é descoberto o MDC (máximo divisor comum) e logo após é feito o cálculo para descobrir o MMC (menor múltiplo comum);
 
 ## 🛠️ Tecnologias e Conceitos 
 Python 3.x e Lógica Matemática - Algoritmo de Euclides📋
